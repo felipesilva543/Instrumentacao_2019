a057e8ddc3932889f8924f3efaf24dfe69a72b85
felipesilva543 <felipesilva543@alu.ufc.br>
2019-11-14 00:57:32 -0300

    Troca da TimerOne pela MsTimer2 | Criação da função primeiraLeitura() | Testes dos sensores | Testes de tudo ligado

--- ProjInstrumentacao/ProjInstrumentacao.ino
+++ ProjInstrumentacao/ProjInstrumentacao.ino
@@ -1,6 +1,7 @@
 //#include <idDHT11.h>
 #include <LiquidCrystal.h>
-#include <TimerOne.h>
+//#include <TimerOne.h>
+#include <MsTimer2.h>
 #include <Servo.h>
 #include "DHT.h"
 
@@ -11,13 +12,12 @@ volatile bool auxSenPre = false; // Ativa o bloco de comandos quando encontra id
 int pos;        // Posição Servo
 
 // Variáveis sensor DHT11
-int lero = 0; // Teste
 float h = 0;  // Umidade
 float t = 0;  // Temperatura
 
 /*######## DEFINES ########*/
-#define pinSenPre    3        // Porta Digital onde o Sinal do Sensor de presença esta conectado
-#define SERVO        9        // Porta Digital onde o Sinal do servo esta conectado
+#define pinSenPre    2        // Porta Digital onde o Sinal do Sensor de presença esta conectado
+#define SERVO        3        // Porta Digital onde o Sinal do servo esta conectado
 #define DHTPIN       A1       // PORTA ANALÓGICA QUE CONECTA O SENSOR DHT11
 #define DHTTYPE      DHT11    // DHT 11
                                          // Intanciação do Objeto Servo Motor
@@ -25,6 +25,7 @@ LiquidCrystal lcd(12, 11, 7, 6, 5, 4);                        // Define os pinos
 DHT dht(DHTPIN, DHTTYPE);
 Servo servo;                                                  // Intanciação do Objeto Servo Motor
 
+int teste = 0;
 void msgDisplay (){
   Serial.println("Interruption ON!");
     lcd.clear();
@@ -35,29 +36,40 @@ void msgDisplay (){
     //Posiciona o cursor na coluna, linha;
     lcd.setCursor(0, 1);
     //Envia o texto entre aspas para o LCD
-    lcd.print("Umid: " + String(h) + " " + String(lero));
+    lcd.print("Umid: " + String(h) + " " + String(teste));
+    teste++;
 }
 
 /*################################# INTERRUPÇÃO SENSOR PRESENÇA #################################*/
 void interruptionPresSensor(){
     auxSenPre = true;     // MUDA A FLAG PARA ATIVAR A SEQUENCIA DEPOIS DE IDETIFICAR A PRESENÇA
-    lero++;
-    Serial.println("Interrupão Pres: " + String(lero));
 }
 
 void ligarVentoinha(int indoVindo){
   digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
   delay(5000);
   digitalWrite(LED_BUILTIN, LOW);   // turn the LED off (LOW is the voltage level)
-  if(indoVindo = 0){
-    servo.write(pos-5);  
-  }else if(indoVindo = 1){
-    servo.write(pos+5);
-  }
+//  if(indoVindo = 0){
+//    servo.write(pos--);  
+//  }else if(indoVindo = 1){
+//    servo.write(pos++);
+//  }
   
   auxSenPre = false;
 }
 
+void primeiraLeitura(){
+  /*Primeria leitura DHT11 e escrita no display */
+  float auxH = dht.readHumidity();
+  float auxT = dht.readTemperature();
+  if (isnan(auxT) || isnan(auxH)){
+    Serial.println("Failed");
+  } else{
+    t = auxT;
+    h = auxH;
+    msgDisplay();
+  }
+}
 
 void setup() {
   Serial.begin(9600);
@@ -65,9 +77,13 @@ void setup() {
   dht.begin();      // Init DHT11
   servo.attach(SERVO); // Init Servo
   pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(8, OUTPUT);
 
-  //Timer1.initialize(10000000);   //TIMER IRA EXECUTAR A CADA 1 SEGUNDO (PARAMETRO EM MICROSEGUNDOS)
-  //Timer1.attachInterrupt(msgDisplay);
+  primeiraLeitura();
+  
+  // Inicialização timer 2
+  MsTimer2::set(5000, msgDisplay); // 500ms period
+  MsTimer2::start();
 
 
 // INTERRUPÇÃO SENSOR DE PRESENÇA
@@ -79,21 +95,21 @@ void setup() {
 
 void loop() {
 /*Leitura sensor Temperatura e Umidade.*/
-//  float auxH = dht.readHumidity();
-//  float auxT = dht.readTemperature();
-//  if (isnan(auxT) || isnan(auxH)){
-//    Serial.println("Failed");
-//  } else{  
-//    Serial.println("T: " + String(auxT));
-//    Serial.println("U: " + String(auxH));
-//    t = auxT;
-//    h = auxH;
-//  }
+  float auxH = dht.readHumidity();
+  float auxT = dht.readTemperature();
+  if (isnan(auxT) || isnan(auxH)){
+    Serial.println("Failed");
+  } else{  
+    Serial.println("T: " + String(auxT));
+    Serial.println("U: " + String(auxH));
+    t = auxT;
+    h = auxH;
+  }
 /*End Leitura sensor Temperatura e Umidade.*/
 
 /*Controle Servo Motor.*/
 
-//for(pos = 0; pos < 180; pos+=5){ //PARA "pos" IGUAL A 0, ENQUANTO "pos" MENOR QUE 180, INCREMENTA "pos"
+//for(pos = 0; pos < 180; pos++){ //PARA "pos" IGUAL A 0, ENQUANTO "pos" MENOR QUE 180, INCREMENTA "pos"
 //  servo.write(pos); //ESCREVE O VALOR DA POSIÇÃO QUE O SERVO DEVE GIRAR
 //  delay(15); //INTERVALO DE 15 MILISSEGUNDOS
 //  if(auxSenPre){
@@ -101,25 +117,28 @@ void loop() {
 //  }
 //}
 //delay(1000); //INTERVALO DE 1 SEGUNDO
-//for(pos = 180; pos >= 0; pos-=5){ //PARA "pos" IGUAL A 180, ENQUANTO "pos" MAIOR OU IGUAL QUE 0, DECREMENTA "pos"
+//for(pos = 180; pos >= 0; pos--){ //PARA "pos" IGUAL A 180, ENQUANTO "pos" MAIOR OU IGUAL QUE 0, DECREMENTA "pos"
 //  servo.write(pos); //ESCREVE O VALOR DA POSIÇÃO QUE O SERVO DEVE GIRAR
 //  delay(15); //INTERVALO DE 15 MILISSEGUNDOS
 //  if(auxSenPre){
 //    ligarVentoinha(1);
 //  }
 //}
-
-servo.write(90);
-
-//for(pos = 0; pos < 180; pos++){ //PARA "pos" IGUAL A 0, ENQUANTO "pos" MENOR QUE 180, INCREMENTA "pos"
-//  servo.write(pos); //ESCREVE O VALOR DA POSIÇÃO QUE O SERVO DEVE GIRAR
-//  delay(15); //INTERVALO DE 15 MILISSEGUNDOS
-//}
 //delay(1000); //INTERVALO DE 1 SEGUNDO
-//for(pos = 180; pos >= 0; pos--){ //PARA "pos" IGUAL A 180, ENQUANTO "pos" MAIOR OU IGUAL QUE 0, DECREMENTA "pos"
-//  servo.write(pos); //ESCREVE O VALOR DA POSIÇÃO QUE O SERVO DEVE GIRAR
-//  delay(15); //INTERVALO DE 15 MILISSEGUNDOS
-//}
+
+
+for(pos = 0; pos < 180; pos++){ //PARA "pos" IGUAL A 0, ENQUANTO "pos" MENOR QUE 180, INCREMENTA "pos"
+  servo.write(pos); //ESCREVE O VALOR DA POSIÇÃO QUE O SERVO DEVE GIRAR
+  Serial.println("For 1");
+  delay(15); //INTERVALO DE 15 MILISSEGUNDOS
+}
+delay(1000); //INTERVALO DE 1 SEGUNDO
+for(pos = 180; pos >= 0; pos--){ //PARA "pos" IGUAL A 180, ENQUANTO "pos" MAIOR OU IGUAL QUE 0, DECREMENTA "pos"
+  servo.write(pos); //ESCREVE O VALOR DA POSIÇÃO QUE O SERVO DEVE GIRAR
+  Serial.println("For 2");
+  delay(15); //INTERVALO DE 15 MILISSEGUNDOS
+}
+delay(1000); //INTERVALO DE 1 SEGUNDO
   
 
   
